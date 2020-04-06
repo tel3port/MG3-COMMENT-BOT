87ad192d9a2adb6640ccf10e314e464b3aa6d3e1
tel3port <carn3lian@gmail.com>
2020-04-07 02:36:28 +0300

    added more static urls

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ print(uuid.uuid4().hex)
 def git_push():
     os.system('git add .')
     os.system('git commit -m "added more static urls"')
-    os.system('git push https://tel3port:AjTdJsetif3Q5dn@MG3-COMMENT-BOT.git --all')
+    os.system('git push https://tel3port:AjTdJsetif3Q5dn@github.com/tel3port/MG3-COMMENT-BOT.git --all')
 
 
 
